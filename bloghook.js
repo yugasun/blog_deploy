00d0147436aae699bc8ca2bb96a81b10120c200c
yugasun <yuga_sun@163.com>
2017-10-23 15:45:53 +0800

    Update bloghook.js

--- bloghook.js
+++ bloghook.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
+
 var http = require('http')
 var spawn = require('child_process').spawn
 var createHandler = require('github-webhook-handler')
 
 // 下面填写的myscrect跟github webhooks配置一样，下一步会说；path是我们访问的路径
-var handler = createHandler({ path: '/auto_build', secret: 'yugasun' })
+var handler = createHandler({ path: '/auto_build', secret: process.env.GITHUB_SECRET_TOKEN })
 
 http.createServer(function (req, res) {
   handler(req, res, function (err) {
